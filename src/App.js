3511ff72dccab2b436d35b53a5b4b5650bbb7eac
Tanzim <tanzim008@gmail.com>
2023-11-02 00:40:52 -0230

    Navbar UI added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from './components/Homepage/Index'
+import Nav from './components/shared/Nav'
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    <Nav/>
+    
+    <Routes>
+        <Route path="/" element={<Homepage/>}> 
+        </Route>
+      </Routes>
+
+    </>
   );
 }
 
--- src/components/shared/Nav.jsx
+++ src/components/shared/Nav.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
-
+import Logo from './../../logo.svg';
+import { Link } from 'react-router-dom';
 const Nav = () => {
   return (
     <>
-      Navabr
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to='/'><img src={Logo} alt='Logo'/></Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item active">
+              <Link className="nav-link" to='/'>Home <span className="sr-only">(current)</span></Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to='/'>About</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to='/'>Menu</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to='/'>Reservation</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to='/'>Order Online</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to='/'>Login</Link>
+            </li>
+          </ul>
+        </div>
+        </div>
+      </nav>
     </>
   )
 }
