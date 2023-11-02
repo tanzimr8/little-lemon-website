0674dbf3569375dfb453beffdd0e090d82ba7d71
Tanzim <tanzim008@gmail.com>
2023-11-02 02:35:34 -0230

    Footer UI added

--- src/components/Homepage/Index.jsx
+++ src/components/Homepage/Index.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from './Header';
 import Specials from './Specials'
 import Testominials from './Testominials';
+import Footer from './../shared/Footer';
 const Index = () => {
   return (
     <>
      <Header/> 
      <Specials/>
      <Testominials/>
+     <Footer/>
     </>
   )
 }
--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
-
+import logo from './../../images/logo.png'
 const Footer = () => {
   return (
     <>
-     Footer 
+    <footer>
+      <div className="footer-col-1">
+        <img src={logo} alt="LOGO" srcset="" />
+      </div>
+      <div className="footer-col-2">
+        <div className="sitemap">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Reservation</li>
+            <li>Order Online</li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-col-3">
+        <div className="contact">
+          <ul>
+            <li>Address: Chicago, US</li>
+            <li>Phone: +1709XXXXXXX</li>
+            <li>Email: hello@littlelemonapp.com</li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-col-4">
+        <div className="social-media">
+          <p>Facebook</p>
+          <p>Instagram</p>
+          <p>Youtube</p>
+        </div>
+      </div>
+    </footer>
+    <div className="footer-bottom">
+        <p>2023 Little Lemon Restaurant. All rights reserved</p>
+    </div>
     </>
   )
 }
