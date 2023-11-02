f4d65b5669e8b57d7443755bcedd361537622295
Tanzim <tanzim008@gmail.com>
2023-11-02 02:21:13 -0230

    testimonial UI  added

--- src/components/Homepage/Index.jsx
+++ src/components/Homepage/Index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from './Header';
 import Specials from './Specials'
+import Testominials from './Testominials';
 const Index = () => {
   return (
     <>
      <Header/> 
      <Specials/>
+     <Testominials/>
     </>
   )
 }
--- src/components/Homepage/Testominials.jsx
+++ src/components/Homepage/Testominials.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from 'react';
+import Avatar1 from './../../images/avatar-1.png'
+import Avatar2 from './../../images/avatar-2.png'
+import Avatar3 from './../../images/avatar-3.png'
 
 const Testominials = () => {
   return (
     <>
-      Testimonials
+      <h2 className='text-center'>Testominials</h2>
+      <div className='testimonials'>
+        <div className="testimonial-card">
+          {/* <div className="rating">Star</div> */}
+          <div className="customer">
+            <div className="customer-avatar">
+              <img src={Avatar1} alt="Avatar"  />
+            </div>
+            <div className="customer-name">Spiderman</div>
+          </div>
+          <div className="review">
+            <blockquote className='blockquote'>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores animi expedita repellat minima, non enim provident aut nulla ipsam doloremque iusto nihil at nisi? Sint et temporibus quaerat accusamus recusandae."</blockquote>
+          </div>
+        </div>
+
+        <div className="testimonial-card">
+          {/* <div className="rating">Star</div> */}
+          <div className="customer">
+            <div className="customer-avatar">
+              <img src={Avatar2} alt="Avatar"  />
+            </div>
+            <div className="customer-name">Black widow</div>
+          </div>
+          <div className="review">
+            <blockquote className='blockquote'>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores animi expedita repellat minima, non enim provident aut nulla ipsam doloremque iusto nihil at nisi? Sint et temporibus quaerat accusamus recusandae."</blockquote>
+          </div>
+        </div>
+
+        <div className="testimonial-card">
+          {/* <div className="rating">Star</div> */}
+          <div className="customer">
+            <div className="customer-avatar">
+              <img src={Avatar3} alt="Avatar"  />
+            </div>
+            <div className="customer-name">Ironman</div>
+          </div>
+          <div className="review">
+            <blockquote className='blockquote'>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores animi expedita repellat minima, non enim provident aut nulla ipsam doloremque iusto nihil at nisi? Sint et temporibus quaerat accusamus recusandae."</blockquote>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
