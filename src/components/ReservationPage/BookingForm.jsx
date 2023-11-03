b0cbd9e19789b9fb487f69a49bc73daa35f124f3
Tanzim <tanzim008@gmail.com>
2023-11-03 01:16:37 -0230

    handle form with useReducer hook

--- src/components/ReservationPage/BookingForm.jsx
+++ src/components/ReservationPage/BookingForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Icon from './../../images/meeting.png'
-const BookingForm = (props) => {
+const BookingForm = ({state,dispatch,availabeTimes,availabeOccassions}) => {
   return (
     <div>
       <div className="form-booking">
@@ -10,15 +10,15 @@ const BookingForm = (props) => {
             <div className="form-group">
                 <div className="input-group">
                     <input id="date" name="date" placeholder="Answer" className="form-control"  type="date"
-                    value = {props.date}
+                    value = {state.date} onChange = {(e)=>{dispatch({type: 'HANDLE DATE CHANGE', payload: e.target.value, field: e.target.name })}}
                     />
                 </div>
             </div>
             <div className="form-group">
                 <div class="input-group">
-                    <select className="form-control" id="sel1">
+                    <select name='time' className="form-control" id="sel1" value={state.time} onChange={(e)=>{dispatch({type:"HANDLE TIME CHANGE", payload:e.target.value, field: e.target.name })}}   >
                         <option selected="true" disabled="disabled">Choose Time</option>
-                        {props.availabeTimes.map((availabeTime)=>{
+                        {availabeTimes.map((availabeTime)=>{
                             return <option>{availabeTime}</option>
                         })}
                     </select>
@@ -26,26 +26,26 @@ const BookingForm = (props) => {
             </div>
             <div className="form-group">
                     <div className="input-group">
-                        <input id="number" name="number" placeholder="Number of guests" className="form-control"  type="number" value = {props.numberOfGuest}/>
+                        <input id="number" name="numberOfGuest" placeholder="Number of guests" min="1" max="10" className="form-control"  type="number" value = {state.numberOfGuest}  onChange={(e)=>{dispatch({type:"HANDLE GUEST NUMBER CHANGE", payload: e.target.value, field: e.target.name})}}/>
                     </div>
                 </div>
             <div className="form-group">
                 <div class="input-group">
-                    <select className="form-control" id="sel1">
+                    <select className="form-control" id="sel1" name='occassions' value={state.occassion}
+                    onChange={(e)=>{dispatch({type:"HANDLE OCCASSION CHANGE", payload: e.target.value, field:e.target.name})}}
+                    >
                         <option selected="true" disabled="disabled">Please Select Occassion</option>
-                        {props.occassions.map((occassion)=>{
+                        {availabeOccassions.map((occassion)=>{
                             return <option>{occassion}</option>
                         })}
                     </select>
                 </div>
             </div>
-                
                 <div className="form-group">
                     <input name="book" className="btn btn-lg btn-success btn-block btnBook" value="BOOK TABLE" type="submit"/>
                 </div>
             </form>
       </div>
-    
     </div>
   )
 }
--- src/components/ReservationPage/Index.jsx
+++ src/components/ReservationPage/Index.jsx
@@ -1,18 +1,57 @@
-import React, { useState } from 'react'
+import React, { useReducer, useState } from 'react'
 import BookingForm from './BookingForm'
+const FormReducer = (state,action)=>{
+  switch(action.type){
+    case "HANDLE DATE CHANGE" : 
+    return {
+      ...state,
+      [action.field]: action.payload,
+    };
+    case "HANDLE TIME CHANGE" : 
+    return {
+      ...state,
+      [action.field]: action.payload,
+    };
+    case "HANDLE GUEST NUMBER CHANGE" : 
+    return {
+      ...state,
+      [action.field]: action.payload,
+    };
+    case "HANDLE OCCASSION CHANGE" : 
+    return {
+      ...state,
+      [action.field]: action.payload,
+    };
+    default : return state;
+  }
+}
+
 const Index = () => {
-  const [date,setDate] = useState('');
-  const [availabeTimes, setAvailabeTimes] = useState(["17:00","18:00","19:00","20:00","21:00"]);
-  const [numberOfGuest,setnumberOfGuest] = useState('');
-  const [occassions, setOccassions] = useState(['Birthday', 'Anniversary','Marriage']);
+  // const [date,setDate] = useState('');
+  // const [availabeTimes, setAvailabeTimes] = useState(["17:00","18:00","19:00","20:00","21:00"]);
+  // const [numberOfGuest,setnumberOfGuest] = useState('');
+  // const [occassions, setOccassions] = useState(['Birthday', 'Anniversary','Marriage']);
+  const availabeTimes= ["17:00","18:00","19:00","20:00","21:00"];
+  const availabeOccassions= ['Birthday', 'Anniversary','Marriage'];
+  
+  const initialFormState = {
+    date : "",
+    time:"",
+    numberOfGuest: '',
+    occassions: ''
+  };
+  const [state,dispatch] = useReducer(FormReducer,initialFormState)
   return (
     <div className='reservation'>
-      <BookingForm 
+      {<BookingForm
+        state={state} dispatch = {dispatch} availabeTimes= {availabeTimes} availabeOccassions= {availabeOccassions}
+      />}
+      {/* <BookingForm 
       date = {date} setDate = {setDate}
       availabeTimes= {availabeTimes} setAvailabeTimes = {setAvailabeTimes}
       numberOfGuest = {numberOfGuest} setnumberOfGuest = {setnumberOfGuest}
       occassions = {occassions} setOccassions = {setOccassions}
-      />
+      /> */}
     </div>
   )
 }
